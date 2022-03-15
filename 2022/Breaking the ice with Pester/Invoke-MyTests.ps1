3c49ab207bed8ccfdb755a0c9a0b9efa95097b9e
codaamok <me@cookadam.co.uk>
2022-03-15 14:27:19 +0000

    Breaking the ice with Pester

--- 2022/Breaking the ice with Pester/Invoke-MyTests.ps1
+++ 2022/Breaking the ice with Pester/Invoke-MyTests.ps1
@@ -1,13 +1,8 @@
 # Call script file containing tests and produce exception if test fails
 # Show configuration and error handling
-$Config = New-PesterConfiguration
-$Config.Run.Path = ".\example.Tests.ps1"
-$Config.Run.Throw = $true
-$Config.Output.Verbosity = "Detailed"
-$Config.TestResult.Enabled = $true
 
 try {
-    Invoke-Pester -Configuration $Config
+    Invoke-Pester -Path '.\example.Tests.ps1'
 }
 catch {
     "Caught"
